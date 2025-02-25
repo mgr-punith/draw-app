c41cb4676284b98c76b3a59d349576a5bb44bf74
Punith Moger <mogerpunith@gmail.com>
2025-02-26 01:34:29 +0530

    structuring

--- apps/drawapp-fe/app/canvas/[roomId]/page.tsx
+++ apps/drawapp-fe/app/canvas/[roomId]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { draw } from "@/app/draw";
 import { useEffect, useRef } from "react";
 
 export default function canvas() {
@@ -7,37 +8,7 @@ export default function canvas() {
 
   useEffect(() => {
     if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-
-      if (!ctx) {
-        return;
-      }
-
-      let clicked = false;
-      let startX = 0;
-      let startY = 0;
-
-      canvas.addEventListener("mousedown", (e) => {
-        clicked = true;
-        startX = e.clientX;
-        startY = e.clientY;
-      });
-
-      canvas.addEventListener("mouseup", (e) => {
-        clicked = false;
-        // console.log(e.clientX);
-        // console.log(e.clientY);
-      });
-
-      canvas.addEventListener("mousemove", (e) => {
-        if (clicked) {
-          const width = e.clientX - startX;
-          const height = e.clientY - startY;
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.strokeRect(startX, startY, width, height);
-        }
-      });
+      draw(canvasRef.current)
     }
   }, [canvasRef]);
 
@@ -45,10 +16,10 @@ export default function canvas() {
     <div className="flex flex-col justify-start min-h-screen items-start">
       <div className="h-[705px] w-[705px]">
         <canvas
-          style={{ backgroundColor: "white" }}
+          className="bg-zinc-950"
           ref={canvasRef}
-          width={700}
-          height={700}
+          width={window.innerWidth}
+          height={window.innerHeight}
         />
       </div>
     </div>
